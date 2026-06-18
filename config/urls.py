9f42264b6c20d3ff1d87d3ef4c76c0e0ba9eeff2
Giovani de Chaves Rigotti <giovanirigotti@SOPRANO.COM.BR>
2026-06-18 10:58:29 -0300

    V0.5.1 - Adicionado logo no cabeçario

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+# 1. Muda o texto que aparece na aba do navegador
+admin.site.site_title = 'Painel de Controle - CMRF '
+
+# 2. Muda o subtítulo na tela inicial do admin (o antigo "Administração do Site")
+admin.site.index_title = 'Controle de usuarios'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_ferramentaria.urls')), 
